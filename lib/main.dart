78ee83927b5bdde083b5eff39fa64e518be24738
R Rifa Fauzi Komara <rifa@cariaja.com>
2019-07-08 17:17:06 +0700

    add simple di, next will implement di with moviedb

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,67 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_simple_dependency_injection/injector.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // ini adalah menempatkan semua pekerjaan inisialisasi injektor ke dalam satu atau lebih modul
+  // sehingga dapat bertindak lebih seperti dependency injector daripada service locator
+  final injector = ModuleContainer().initialise(Injector.getInjector());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  // NOTE: ini adalah yang terbaik untuk merancang kode Anda sehingga Anda tidak perlu untuk
+  // berinteraksi dengan injektor itu sendiri.  Jadikan framework ini bertindak seperti depencendy injector
+  // dengan menyuntikkan dependencies injected ke objek di konstruktornya. Dengan begitu Anda terhindar
+  // dari ikatan erat dengan injektor itu sendiri.
+
+  // Penggunaan dasar, bagaimanapun, pasangan ketat semacam ini dan interaksi langsung dengan injektor
+  // harus dibatasi.  Sebagai gantinya lebih suka dependencies injection.
+
+  // Pengambilan dependency sederhana and memanggil method
+  injector.get<SomeService>().doSomething();
+
+  // dapatkan instance dari masing-masing jenis yang dipetakan yang sama
+  final instances = injector.getAll<SomeType>();
+  print(instances.length); // prints '3'
+
+  // menyampaikan argumen tambahan saat menerima contoh
+  final instance = injector.get<SomeOtherType>(additionalParameters: {"id": "some-id"});
+  print(instance.id); // prints 'some-id'
+}
+
+class ModuleContainer {
+  Injector initialise(Injector injector) {
+    injector.map<Logger>((i) => Logger(), isSingleton: true);
+    injector.map<String>((i) => "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata", key: "apiUrl");
+    injector.map<SomeService>((i) => SomeService(i.get<Logger>(), i.get<String>(key: "apiUrl")));
+
+    injector.map<SomeType>((injector) => SomeType("0"));
+    injector.map<SomeType>((injector) => SomeType("1"), key: "One");
+    injector.map<SomeType>((injector) => SomeType("2"), key: "Two");
+
+    injector.mapWithParams<SomeOtherType>((i, p) => SomeOtherType(p["id"]));
+
+    return injector;
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class Logger {
+  void log(String message) => print(message);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SomeService {
+  final Logger _logger;
+  final String _apiUrl;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  SomeService(this._logger, this._apiUrl);
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void doSomething() {
+    _logger.log("Doing something with the api at '$_apiUrl'");
   }
+}
+
+class SomeType {
+  final String id;
+  SomeType(this.id);
+}
+
+class SomeOtherType {
+  final String id;
+  SomeOtherType(this.id);
 }
